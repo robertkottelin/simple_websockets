9b5577a21afd2d299f529e64776ee982a82198b1
robertkottelin <robert.kottelin@gmail.com>
2023-05-01 14:07:22 +0200

    username and client-to-client messaging implemented

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,63 @@ use simple_websockets::{Event, Responder};
 use std::collections::HashMap;
 
 fn main() {
-    // listen for WebSockets on port 8080:
-    let event_hub = simple_websockets::launch(8080)
-        .expect("failed to listen on port 8080");
-    // map between client ids and the client's `Responder`:
-    let mut clients: HashMap<u64, Responder> = HashMap::new();
+    let event_hub = simple_websockets::launch(8080).expect("failed to listen on port 8080");
+    let mut clients: HashMap<u64, (Option<String>, Responder)> = HashMap::new();
+    let mut messages: HashMap<String, Vec<String>> = HashMap::new();
 
     loop {
         match event_hub.poll_event() {
             Event::Connect(client_id, responder) => {
                 println!("A client connected with id #{}", client_id);
-                // add their Responder to our `clients` map:
-                clients.insert(client_id, responder);
-            },
+                clients.insert(client_id, (None, responder.clone()));
+                responder.send(simple_websockets::Message::Text(
+                    "Please choose a username:".to_string(),
+                ));
+            }
             Event::Disconnect(client_id) => {
                 println!("Client #{} disconnected.", client_id);
-                // remove the disconnected client from the clients map:
                 clients.remove(&client_id);
-            },
+            }
             Event::Message(client_id, message) => {
-                println!("Received a message from client #{}: {:?}", client_id, message);
-                // retrieve this client's `Responder`:
-                let responder = clients.get(&client_id).unwrap();
-                // echo the message back:
-                responder.send(message);
-            },
+                if let Some((username, _)) = clients.get(&client_id) {
+                    let msg_str = match message {
+                        simple_websockets::Message::Text(text) => text,
+                        _ => continue,
+                    };
+
+                    if username.is_none() {
+                        clients.get_mut(&client_id).unwrap().0 = Some(msg_str.clone());
+                        clients.get_mut(&client_id).unwrap().1.send(
+                            simple_websockets::Message::Text(format!(
+                                "Username set to '{}'",
+                                msg_str
+                            )),
+                        );
+                    } else {
+                        let user = username.as_ref().unwrap();
+                        println!(
+                            "Received a message from client #{} ({}): {:?}",
+                            client_id, user, msg_str
+                        );
+                        messages
+                            .entry(user.to_string())
+                            .or_default()
+                            .push(msg_str.clone());
+
+                        // Broadcast message to all clients except the sender
+                        for (other_client_id, (_, other_responder)) in &clients {
+                            if *other_client_id != client_id {
+                                // Construct the message inside the loop
+                                let broadcast_msg = simple_websockets::Message::Text(format!(
+                                    "{}: {}",
+                                    user, msg_str
+                                ));
+                                other_responder.send(broadcast_msg);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
