66e6ff74ba736ad34a423bf715f145147abd381a
Robert Kottelin <robert.kottelin@gmail.com>
2023-05-06 11:36:43 +0200

    also displaying sent messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
 import ReconnectingWebSocket from "reconnecting-websocket";
 
 const Chat = () => {
   const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  //const [sentMessages, setSentMessages] = useState([]); // [message, message, ...
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     const ws = new ReconnectingWebSocket("ws://localhost:8080");
     setSocket(ws);
+
+    ws.onmessage = (event) => {
+      setMessages((prevMessages) => [...prevMessages, event.data]);
+    };
+
     return () => {
       ws.close();
     };
   }, []);
 
-  // Rest of the component logic goes here
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      socket.send(input);
+      setMessages((prevMessages) => [...prevMessages, input]);
+      setInput('');
+    }
+  };
 
   return (
     <div>
-      {/* Chat UI elements go here */}
+      <h2>Chat</h2>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
 
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Chat />
     </div>
-    
   );
 }
 
